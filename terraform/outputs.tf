94fedaaaf61c9e74daec95aae4c01705fd29f079
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-19 13:17:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,3 @@
-output "ecr_url" {
-  value = aws_ecr_repository.strapi.repository_url
-}
-
-output "ecs_cluster" {
-  value = aws_ecs_cluster.strapi.name
-}
-
-output "ecs_service" {
-  value = aws_ecs_service.strapi.name
-}
-
-output "strapi_task_count" {
-  value = aws_ecs_service.strapi.desired_count
+output "alb_url" {
+  value = aws_lb.alb.dns_name
 }
