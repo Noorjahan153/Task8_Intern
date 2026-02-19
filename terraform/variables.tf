e2209a16abda4e29d0b6668c1dc5d20de3bef653
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-19 13:13:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,3 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "ecs_desired_count" {
-  description = "Number of running Strapi tasks"
-  type        = number
-  default     = 1
-}
-
-variable "strapi_port" {
-  description = "Port for Strapi"
-  type        = number
-  default     = 1337
+variable "region" {
+  default = "ap-south-1"
 }
