61aad3adec31aef06ef1d4ef5480637643e04a95
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-19 11:38:54 +0530

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "db_password" { type = string, sensitive = true }
+variable "app_keys" { type = string, sensitive = true }
+variable "api_token_salt" { type = string, sensitive = true }
+variable "admin_jwt_secret" { type = string, sensitive = true }
+variable "transfer_token_salt" { type = string, sensitive = true }
+variable "jwt_secret" { type = string, sensitive = true }
