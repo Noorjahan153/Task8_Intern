b3b8186ada029cfec6dc920e46c97f7d789e1167
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-19 12:27:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,11 @@
-output "strapi_url" {
-  description = "The public URL of Strapi"
-  value       = aws_lb.alb.dns_name
+output "ecr_url" {
+  value = aws_ecr_repository.strapi.repository_url
+}
+
+output "ecs_cluster" {
+  value = aws_ecs_cluster.strapi.name
+}
+
+output "ecs_service" {
+  value = aws_ecs_service.strapi.name
 }
